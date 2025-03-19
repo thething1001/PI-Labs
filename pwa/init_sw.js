7ecc8d1b4f0c063727ef2fdec010e330391487da
Bohdan Hamela <bohdan.hamela.pz.2023@lpnu.ua>
2025-03-19 17:16:07 +0200

    Lab02 - Fix bugs reletated to pwa

--- pwa/init_sw.js
+++ pwa/init_sw.js
@@ -1,6 +1,11 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(() => console.log("Service Worker registered"))
-    .catch((err) => console.error("Service Worker registration failed", err));
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
 }
--- sw.js
+++ sw.js
@@ -1,26 +1,37 @@
-const CACHE_NAME = "pwa-cms-cache-v1";
+const CACHE_NAME = "pwa-cms-cache";
 const ASSETS = [
   "/",
   "/students",
   "/students/students.html",
   "/students/students.css",
   "/students/students.js",
+
   "/dashboard",
   "/dashboard/dashboard.html",
   "/dashboard/dashboard.css",
   "/dashboard/dashboard.js",
+
   "/tasks",
   "/tasks/tasks.html",
   "/tasks/tasks.css",
   "/tasks/tasks.js",
+
   "/messages",
   "/messages/messages.html",
   "/messages/messages.css",
   "/messages/messages.js",
+
   "/global.css",
   "/navbar.css",
   "/header.css",
   "/header.js",
+  "/offline.html",
+  "/sw.js",
+
+  "/pwa",
+  "/pwa/init_sw.js",
+  "/pwa/manifest.json",
+
   "/assets",
   "/assets/logo.svg",
   "/assets/add.svg",
@@ -37,30 +48,36 @@ const ASSETS = [
   "/assets/students.svg",
   "/assets/tasks.svg",
   "/assets/trash.svg",
-  "/assets/icon-128.png",
-  "/assets/icon-512.png",
+  "/assets/logo-128.png",
+  "/assets/logo-192.png",
+  "/assets/logo-256.png",
+  "/assets/logo-512.png",
+  "/assets/screenshot-wide.png",
+  "/assets/screenshot-narrow.png"
 ];
 
 // Встановлення Service Worker та кешування файлів
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching files');
       return cache.addAll(ASSETS);
     })
   );
 });
 
 // Перехоплення запитів і завантаження з кешу
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/offline.html'))
   );
 });
 
 // Оновлення Service Worker і видалення старого кешу
 self.addEventListener("activate", (event) => {
+  console.log('Updating cache');
   event.waitUntil(
     caches
       .keys()

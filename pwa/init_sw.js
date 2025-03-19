9eef761ca59eacadef62b1eb794028fc187e6fe9
Bohdan Hamela <bohdan.hamela.pz.2023@lpnu.ua>
2025-03-19 17:18:04 +0200

    Lab02 - Fix bugs reletated to pwa 2

--- pwa/init_sw.js
+++ pwa/init_sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('PI_Labs/sw.js')
       .then(registration => {
         console.log('Service Worker registered:', registration);
       })
